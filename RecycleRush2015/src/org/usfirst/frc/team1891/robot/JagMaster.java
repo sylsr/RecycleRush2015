040fcff88360cf48d697b9aba066b8cd635a5c40
Tyler <sylsr9@gmail.com>
2015-02-26 17:23:52 -0700

    Added simple JagMaster

--- RecycleRush2015/src/org/usfirst/frc/team1891/robot/JagMaster.java
+++ RecycleRush2015/src/org/usfirst/frc/team1891/robot/JagMaster.java
@@ -1,5 +1,30 @@
 package org.usfirst.frc.team1891.robot;
 
-public class JagMaster {
+import edu.wpi.first.wpilibj.CANJaguar;
 
+public class JagMaster 
+{
+	CANJaguar jag2;
+	CANJaguar jag3;
+	CANJaguar jag4;
+	CANJaguar jag6;
+	public JagMaster()
+	{
+		jag2=new CANJaguar(2);
+		jag3=new CANJaguar(3);
+		jag4=new CANJaguar(4);
+		jag6=new CANJaguar(6);
+	}
+	public void setAllVoltage()
+	{
+		jag2.setVoltageMode();
+		jag3.setVoltageMode();
+		jag4.setVoltageMode();
+		jag6.setVoltageMode();
+		jag2.enableControl();
+		jag3.enableControl();
+		jag4.enableControl();
+		jag6.enableControl();
+	}
+	
 }
