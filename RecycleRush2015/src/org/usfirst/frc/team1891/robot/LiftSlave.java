93257a981ba775a05c0acafb7678b842c365789d
Zach <tinvariable578@gmail.com>
2015-02-26 19:23:12 -0700

    More Lift

--- RecycleRush2015/src/org/usfirst/frc/team1891/robot/LiftSlave.java
+++ RecycleRush2015/src/org/usfirst/frc/team1891/robot/LiftSlave.java
@@ -1,5 +1,16 @@
 package org.usfirst.frc.team1891.robot;
 
-public class LiftSlave {
+public class LiftSlave 
+{
+	LiftMaster jagLift;
+	public LiftSlave()
+	{
+		jagLift = new LiftMaster();
+	}
+	public void liftUp()
+	{
+		jagLift.setVoltage();
+		jagLift.jagLiftMove(-2);
+	}
 
 }
