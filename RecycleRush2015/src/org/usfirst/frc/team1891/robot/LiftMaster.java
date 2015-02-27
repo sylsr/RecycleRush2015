4d89c9d7bc1d6fb29c910cd560f48d4a56fd74fd
Zach <tinvariable578@gmail.com>
2015-02-26 17:42:47 -0700

    Lift Code

--- RecycleRush2015/src/org/usfirst/frc/team1891/robot/LiftMaster.java
+++ RecycleRush2015/src/org/usfirst/frc/team1891/robot/LiftMaster.java
@@ -1,5 +1,20 @@
 package org.usfirst.frc.team1891.robot;
 
-public class LiftMaster {
+import edu.wpi.first.wpilibj.CANJaguar;
+
+public class LiftMaster 
+{
+	CANJaguar jagLift;
+	public LiftMaster()
+	{
+		LifterJag jagLift = new LifterJag();
+	}
+	public double Init()
+	{
+		if(jagLift.getForwardLimitOK() != false)
+		{
+			jagLift.enableControl();
+		}
+	}
 
 }
--- RecycleRush2015/src/org/usfirst/frc/team1891/robot/LifterJag.java
+++ RecycleRush2015/src/org/usfirst/frc/team1891/robot/LifterJag.java
@@ -1,5 +1,29 @@
 package org.usfirst.frc.team1891.robot;
 
-public class LifterJag {
+import edu.wpi.first.wpilibj.CANJaguar;
+
+public class LifterJag 
+{
+	CANJaguar jagLift;
+	
+	public LifterJag()
+	{
+		jagLift = new CANJaguar(5);
+	}
+	
+	public void setVoltageMode()
+	{
+		jagLift.setVoltageMode();
+	}
+	public void setPositionMode()
+	{
+		jagLift.setPositionMode(CANJaguar.kQuadEncoder, 1000, 0, 0, 0);
+	}
+	public void jagLiftMove()
+	{
+		
+	}
+	
+	
 
 }
